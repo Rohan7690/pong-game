00cfbc3af59d8dd8fb27be0b1656f7797d593ab4
Rohan kumar <109895122+Rohan7690@users.noreply.github.com>
2022-10-08 21:44:31 +0530

    Create paddle.py

---
+++ paddle.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+screen = Screen()
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        self.new_y = self.ycor() + 20
+        self.goto(self.xcor(), self.new_y)
+
+    def go_down(self):
+        self.new_y = self.ycor() - 20
+        self.goto(self.xcor(), self.new_y)
+
+    def l_up(self):
+        self.new_y = self.ycor() + 20
+        self.goto(self.xcor(), self.new_y)
+
+    def l_down(self):
+        self.new_y = self.ycor() - 20
+        self.goto(self.xcor(), self.new_y)
+
+
